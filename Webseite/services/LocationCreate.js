26ccc48568d54506e9e91ea15df7005e3637052a
AlexanderMazurowski <99261467+AlexanderMazurowski@users.noreply.github.com>
2022-03-26 00:42:23 +0100

    commit

---
+++ Webseite/services/LocationCreate.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//Mustafa Demir’s Gemüse Kebap
+//https://www.google.com/maps/place/Mustafa+Demir%E2%80%99s+Gem%C3%BCse+Kebap/@52.5094884,13.4497155,17.83z/data=!3m1!5s0x47a84e5b7a719811:0x810c919bcf16dd8d!4m5!3m4!1s0x47a84e58e23b7319:0x8069096b50d11cb6!8m2!3d52.5100384!4d13.451065
+//döner
+//Montag,11:00–02:00
+let distance;
+class Location {
+  constructor(name, tags, gmapslink, time, curLoc) {
+    this.name = name;
+    this.tags = tags;
+    this.gmapslink = gmapslink;
+    this.time = time;
+    this.distance = "";
+  }
+  async getCordinatesAndDistance() {
+    const lat = this.gmapslink.split("@")[1].split(",")[0]; //nordsüd
+    const lon = this.gmapslink.split("@")[1].split(",")[1]; //westeast
+    let cord;
+
+    function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2 - lat1); // deg2rad below
+      var dLon = deg2rad(lon2 - lon1);
+      var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) *
+          Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c; // Distance in km
+      return d;
+    }
+    function deg2rad(deg) {
+      return deg * (Math.PI / 180);
+    }
+
+    this.distance = distance;
+    return (
+      `https://www.google.com/maps/@${this.lat},${this.lon},12z` + this.distance
+    );
+  }
+}
