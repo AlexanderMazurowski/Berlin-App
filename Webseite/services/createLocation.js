b81b2d7db71b26e3c84fc6ea511d14ca0e6ffa59
AlexanderMazurowski <99261467+AlexanderMazurowski@users.noreply.github.com>
2022-04-08 18:41:30 +0200

    fix

--- Webseite/services/createLocation.js
+++ Webseite/services/createLocation.js
@@ -4,7 +4,7 @@ function createLocations() {
   let finalLocation = [];
   for (let i = 0; i < locations.length; i++) {
     finalLocation[i] = new Location(locations[i], latlon);
-    //finalLocation[i].getCordinatesAndDistance();
+    finalLocation[i].getCordinatesAndDistance();
     console.log(finalLocation[i].name);
     //calculateDistance(finalLocation[i]);
   }
