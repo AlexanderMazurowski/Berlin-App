7935d424bb734a2b577986778d3b8794a8ddc8db
AlexanderMazurowski <99261467+AlexanderMazurowski@users.noreply.github.com>
2022-04-08 17:18:09 +0200

    new filter

--- Webseite/services/filters.js
+++ Webseite/services/filters.js
@@ -1,26 +1,40 @@
 "use strict";
 
+function setfilterstyle(domlabel) {
+  const selctfilter = document.querySelector(".filter-choose-select");
+
+  selctfilter.remove("filter-choose-select");
+  selctfilter.add("filter-choose");
+  domlabel.remove("filter-choose");
+  domlabel.add("filter-choose-select");
+}
+
 function filterSetting(filter) {
-  document
-    .querySelector("#filterele")
-    .insertAdjacentHTML(
-      "afterbegin",
-      `<label class="filter-choose"><input type="radio" name="filter">${filter.toLowerCase()}">${filter}</label>`
-    );
+  document.querySelector(".filter-pop-up").insertAdjacentHTML(
+    "afterbegin",
+    `<label class="filter-choose" id="${filter.toLowerCase()}label"><input class="hide" type="radio" name="filter" id="${filter.toLowerCase()}"> ${filter} </label>`
+
+    //`<label class="filter-choose"><input type="radio" name="filter">${filter.toLowerCase()}">${filter}</label>`
+  );
+  const domlabel = document.querySelector(`#${filter.toLowerCase()}label`);
   document
     .querySelector(`#${filter.toLowerCase()}`)
     .addEventListener("click", function () {
       if (filter !== "none") {
         console.log("Checkbox is checked..");
+
         locint.forEach((loc) => {
           const isVisible = loc.tags.includes(filter);
           //console.log(loc.name.toLowerCase());
           loc.hide = !isVisible;
         });
         displayLocSearch(locint);
+        setfilterstyle(domlabel);
       } else {
         console.log("Checkbox is not checked..");
         displayLoc(locint);
+
+        setfilterstyle(domlabel);
       }
     });
 }
@@ -40,9 +54,9 @@ const alltags = [
   loct,
   msum,
   h,
-  club,
   park,
   important,
+  "none",
 ];
 
 for (let i = 0; i < alltags.length; i++) {
